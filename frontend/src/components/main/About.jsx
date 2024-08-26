54ec081539c98d4cd03f2892052013206ebe16e1
M-Roseingrave <163070612+M-Roseingrave@users.noreply.github.com>
2024-08-26 18:36:33 -0400

    final edits

--- frontend/src/components/main/About.jsx
+++ frontend/src/components/main/About.jsx
@@ -17,7 +17,6 @@ export default function About() {
             textAlign: "center",
             marginLeft: "1vw",
             marginTop: "5vh",
-
           }}
         >
           <h4>
@@ -37,7 +36,7 @@ export default function About() {
                 justifyContent: "center",
                 alignContent: "center",
                 border: "1px solid black",
-                padding: "20px"
+                padding: "20px",
               }}
             >
               <div
@@ -134,39 +133,41 @@ export default function About() {
                 <b>Player Three Bio</b>
               </p>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                at felis semper, tempus mi id, feugiat orci. Donec auctor
-                pellentesque tellus ac dapibus. Donec imperdiet, purus id
-                lacinia ultricies, nisi neque faucibus sem, non dictum mauris
-                nisi a nisl. Aliquam luctus rhoncus dui at egestas. Integer urna
-                enim, tincidunt et lectus eu, posuere pulvinar lectus. Proin
-                interdum purus finibus ornare iaculis. Quisque placerat neque ac
-                elementum maximus. In magna ligula, tincidunt sit amet finibus
-                ultrices, posuere at erat. In ut eleifend risus, tristique
-                lacinia turpis. Etiam eleifend finibus erat nec luctus. Cras
-                consequat consequat erat, non volutpat nisi vehicula sit.
-                </p>
+                Morrigan is currently making the career transition from customer
+                service to a software developer! Having worked in a bakery, a
+                deli, forged from the kitchens of the Vermont College of Fine
+                Arts and even as a night auditor of a well known hotel chain,
+                she is adaptable and well equipped to deal with public relations
+                and various situations! Well versed in javascript, she enjoys
+                creating the backend working such as routes and controllers and
+                elements of physical design aspect such as placement, colors,
+                and themes. She plans to be a remote software engineer for the
+                freedom of working from anywhere. When not coding, Morrigan
+                spends her time playing video games with her friends, editing
+                videos and writing fantasy.
+              </p>
+              <a href="linkedin.com/in/morrigan-roseingrave-42407b324">Check out her LinkedIn here!</a>
             </div>
           </div>
-        <div
-          style={{
-            display: "block",
-            float: "right",
-            marginTop: "1vh"
-          }}
-        >
-          <Button
-          size="xs"
-            type="click"
-            onClick={() => navigate("/")}
+          <div
             style={{
-              backgroundColor: "#ff532f",
-              color: "white"
+              display: "block",
+              float: "right",
+              marginTop: "1vh",
             }}
           >
-            Back
-          </Button>
-        </div>
+            <Button
+              size="xs"
+              type="click"
+              onClick={() => navigate("/")}
+              style={{
+                backgroundColor: "#ff532f",
+                color: "white",
+              }}
+            >
+              Back
+            </Button>
+          </div>
         </div>
       </div>
     </>
